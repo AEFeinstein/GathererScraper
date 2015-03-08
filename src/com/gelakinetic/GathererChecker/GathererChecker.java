d78e8b563aebbd296c5266c02c8e1975a6d93927
AEFeinstein <gelakinetic@gmail.com>
2015-03-08 17:11:21 -0400

    Added checks for judge docs Formatting

--- src/com/gelakinetic/GathererChecker/GathererChecker.java
+++ src/com/gelakinetic/GathererChecker/GathererChecker.java
@@ -24,25 +24,26 @@ import com.gelakinetic.GathererScraper.GathererScraper;
 
 public class GathererChecker {
 	/**
-	 * Main function. It reads in an RSS file, scrapes a list of sets from Gatherer,
-	 * and writes a new RSS file if anything changed
+	 * Main function. It reads in an RSS file, scrapes a list of sets from
+	 * Gatherer, and writes a new RSS file if anything changed
 	 * 
-	 * @param args unused
+	 * @param args
+	 *            unused
 	 */
 	public static void main(String[] args) {
 		try {
 			File rssFile = new File(args[0]);
-			
+
 			boolean changes = false;
 			/* Read in the current RSS file */
-			ArrayList<RssEntry>		entries		= readRssFile(rssFile);
+			ArrayList<RssEntry> entries = readRssFile(rssFile);
 			/* Scrape a list of expansions */
-			ArrayList<Expansion>	expansions	= GathererScraper.scrapeExpansionList();
+			ArrayList<Expansion> expansions = GathererScraper.scrapeExpansionList();
 
 			/* For each expansion */
-			for(Expansion exp : expansions) {
+			for (Expansion exp : expansions) {
 				/* See if it's already in the RSS file */
-				if(!entries.contains(exp)) {
+				if (!entries.contains(exp)) {
 					/* If it isn't, add it to the file. Null GUID and date will be populated */
 					entries.add(new RssEntry(exp.mName_gatherer, null, null, null));
 					changes = true;
@@ -52,25 +53,35 @@ public class GathererChecker {
 			/* Get the latest rules, and make an RssEntry for it */
 			RssEntry latestRules = GetLatestRules();
 			/* If the entries to not have the latest rules */
-			if(!entries.contains(latestRules)) {
+			if (!entries.contains(latestRules)) {
 				/* Add them */
 				entries.add(latestRules);
 				changes = true;
 			}
 
+			/* Get the latest judge docs, and make RssEntries for them */
+			for (RssEntry judgeDoc : GetJudgeDocs()) {
+				/* If the entries do not have the latest judge docs */
+				if (!entries.contains(judgeDoc)) {
+					/* Add them */
+					entries.add(judgeDoc);
+					changes = true;
+				}
+			}
+
 			/* If there are changes, write the new RSS file */
-			if(changes) {
+			if (changes) {
 				/* First delete the file */
-				if(rssFile.exists()) {
+				if (rssFile.exists()) {
 					rssFile.delete();
 				}
-				
-				FileOutputStream fos = new FileOutputStream(rssFile, false); 
+
+				FileOutputStream fos = new FileOutputStream(rssFile, false);
 				FileChannel channel = fos.getChannel();
 
 				/* Writes a sequence of bytes to this channel from the given buffer. */
 				channel.write(GetRssHeader());
-				for(RssEntry entry : entries) {
+				for (RssEntry entry : entries) {
 					channel.write(entry.getRssEntry());
 				}
 				channel.write(GetRssFooter());
@@ -89,29 +100,30 @@ public class GathererChecker {
 	 * @return a UTF-8 encoded header for an RSS file
 	 */
 	public static ByteBuffer GetRssHeader() {
-		return Charset.forName("UTF-8").encode(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
+		return Charset
+			.forName("UTF-8")
+			.encode("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
 				+ "<rss version=\"2.0\">\n"
 				+ "<channel>\n"
 				+ "\t<title>Gatherer's Available Sets</title>\n"
 				+ "\t<link>http://gatherer.wizards.com/Pages/Default.aspx</link>\n"
-				+ "\t<description>A dinky little feed to notify me when new sets are posted to Gatherer</description>\n");
+				+ "\t<description>A dinky little feed to notify me when new sets are posted to Gatherer"
+				+ "</description>\n");
 	}
 
 	/**
 	 * @return a UTF-8 encoded footer for an RSS file
 	 */
 	public static ByteBuffer GetRssFooter() {
-		return Charset.forName("UTF-8").encode(
-				"</channel>\n" +
-				"</rss>\n");
+		return Charset.forName("UTF-8").encode("</channel>\n</rss>\n");
 	}
 
 	/**
 	 * Reads the given RSS file and returns an ArrayList of it's contents
 	 * 
-	 * @param rssFile	The RSS file to read in
-	 * @return			An ArrayList<RssEntry> with the given file's information
+	 * @param rssFile
+	 *            The RSS file to read in
+	 * @return An ArrayList<RssEntry> with the given file's information
 	 */
 	public static ArrayList<RssEntry> readRssFile(File rssFile) {
 		ArrayList<RssEntry> rssEntries = new ArrayList<>();
@@ -135,7 +147,7 @@ public class GathererChecker {
 							eElement.getElementsByTagName(RssEntry.TITLE).item(0).getTextContent(),
 							eElement.getElementsByTagName(RssEntry.GUID).item(0).getTextContent(),
 							eElement.getElementsByTagName(RssEntry.PUBDATE).item(0).getTextContent(),
-							null));
+							eElement.getElementsByTagName(RssEntry.LINK).item(0).getTextContent()));
 				}
 			}
 		}
@@ -144,12 +156,18 @@ public class GathererChecker {
 		}
 		return rssEntries;
 	}
-	
-	public static RssEntry GetLatestRules () throws IOException {
+
+	/**
+	 * Look at the page that has the comprehensive rules, get the url, pick the date out of it, make an RssEntry for it
+	 * and return it
+	 * 
+	 * @return An RssEntry for the latest comprehensive rules
+	 * @throws IOException If the program has trouble reading the webpage
+	 */
+	public static RssEntry GetLatestRules() throws IOException {
 		/* One big line to get the webpage, then the element, then the attribute for the comprehensive rules url */
 		String url = Jsoup.connect("http://magic.wizards.com/en/gameinfo/gameplay/formats/comprehensiverules").get()
-				.getElementsByAttributeValueContaining("href", "txt").get(0)
-				.attr("href");
+				.getElementsByAttributeValueContaining("href", "txt").get(0).attr("href");
 
 		/* Pick the date out of the link */
 		String dateSubStr = url.substring(url.length() - 12, url.length() - 4);
@@ -158,9 +176,86 @@ public class GathererChecker {
 		cal.set(Integer.parseInt(dateSubStr.substring(0, 4)),
 				Integer.parseInt(dateSubStr.substring(4, 6)) - 1,
 				Integer.parseInt(dateSubStr.substring(6, 8)));
-		String dateStr = new SimpleDateFormat("MM/dd/yyyy").format(cal.getTime());
-		
+		String dateStr = new SimpleDateFormat("yyyy/MM/dd HH:mm").format(cal.getTime());
+
 		/* Make the RssEntry and return it */
 		return new RssEntry("Comprehensive Rules " + dateStr, null, dateStr, url);
+	}
+
+	/**
+	 * Look at the page that has the judge documents, get the date, make RssEntries for the documents, and return them
+	 * 
+	 * @return An ArrayList of RssEntries for all the judge documents
+	 * @throws IOException
+	 */
+	public static ArrayList<RssEntry> GetJudgeDocs() throws IOException {
+		/* Pick the date out of the bluewizard website */
+		String date = Jsoup.connect("http://www.bluewizard.net/judgeDocs.html").get()
+				.getElementsByAttributeValueContaining("class", "media-body").get(0).text();
+
+		/* Try real hard to prettify the date. Not really necessary */
+		String dateStr;
+		try {
+			date = date.substring(date.indexOf("(") + 1, date.indexOf(")"));
+			String dateParts[] = date.split("\\s+");
+			int month = 0;
+			switch (dateParts[1]) {
+				case "January":
+					month = 0;
+					break;
+				case "February":
+					month = 1;
+					break;
+				case "March":
+					month = 2;
+					break;
+				case "April":
+					month = 3;
+					break;
+				case "May":
+					month = 4;
+					break;
+				case "June":
+					month = 5;
+					break;
+				case "July":
+					month = 6;
+					break;
+				case "August":
+					month = 7;
+					break;
+				case "September":
+					month = 8;
+					break;
+				case "October":
+					month = 9;
+					break;
+				case "November":
+					month = 10;
+					break;
+				case "December":
+					month = 11;
+					break;
+			}
+			Calendar cal = Calendar.getInstance();
+			cal.clear();
+			cal.set(Integer.parseInt(dateParts[2]), month, Integer.parseInt(dateParts[0]));
+			dateStr = new SimpleDateFormat("yyyy/MM/dd HH:mm").format(cal.getTime());
+		}
+		catch (Exception e) {
+			/* In case parsing fails */
+			dateStr = date;
+		}
+
+		/* Make RssEntries for each of the three judge documents */
+		ArrayList<RssEntry> entries = new ArrayList<RssEntry>();
+		entries.add(new RssEntry("Magic Infraction Guide, " + date, null, dateStr,
+				"http://www.bluewizard.net/docs/epub/MagicInfractionGuide.epub"));
+		entries.add(new RssEntry("Magic Tournament Rules, " + date, null, dateStr,
+				"http://www.bluewizard.net/docs/epub/MagicTournamentRules.epub"));
+		entries.add(new RssEntry("Judging At Regular, " + date, null, dateStr,
+				"http://www.bluewizard.net/docs/epub/JudgingAtRegular.epub"));
+
+		return entries;
 	}
 }
