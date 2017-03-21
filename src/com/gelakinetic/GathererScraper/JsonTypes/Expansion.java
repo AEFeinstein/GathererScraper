352ccf3e297583f60f598b45bef0b9981e848ec6
Adam <gelakinetic@gmail.com>
2017-03-21 00:18:44 -0400

    Formatting

--- src/com/gelakinetic/GathererScraper/JsonTypes/Expansion.java
+++ src/com/gelakinetic/GathererScraper/JsonTypes/Expansion.java
@@ -10,25 +10,25 @@ public class Expansion {
 
     // Name used by Gatherer
     public String mName_gatherer = "";
-    
+
     // expansion code used by Gatherer
     public String mCode_gatherer = "";
-    
+
     // expansion code used by magiccards.info
     public String mCode_mtgi = "";
-    
+
     // expansion mName used by TCGPlayer.com
     public String mName_tcgp = "";
-    
-	// expansion name used by MagicCardMarket.eu
-	public String mName_mkm		= "";
-	
+
+    // expansion name used by MagicCardMarket.eu
+    public String mName_mkm = "";
+
     // Date the expansion was released
     public long mReleaseTimestamp = 0;
-    
+
     // Whether or not this expansion has foil cards
     public boolean mCanBeFoil = false;
-    
+
     // MD5 digest for scraped cards, to see when things change
     public String mDigest = "";
 }
